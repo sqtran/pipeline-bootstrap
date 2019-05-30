06318fac203e77b0973f060a69fbe83d54dbce37
Steve Tran <stran@redhat.com>
2019-05-30 10:27:49 -0400

    Test with stage block

--- src/com/steve/ocp/NewClass.groovy
+++ src/com/steve/ocp/NewClass.groovy
@@ -5,6 +5,10 @@ println "Hello World from New Class!"
 
   node {
     println "Hello World from inside node block"
+
+    stage("Steve 1") {
+      println "Hello World from inside stage block"
+    }
   }
 
 
