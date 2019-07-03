fe824877367e51f016d3fddf8065f224eb88ae00
Steve Tran <stran@redhat.com>
2019-07-03 10:48:44 -0400

    Add tag and SHA retrieval to Release Pipeline

--- src/com/steve/ocp/ReleasePipeline.groovy
+++ src/com/steve/ocp/ReleasePipeline.groovy
@@ -2,31 +2,47 @@ package com.steve.ocp
 
 def process(def params) {
 
-	ocHome  = tool 'oc311'
-	ocHome  = "$ocHome/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit"
-	oc      = "$ocHome/oc"
-
-  // TODO checkout by GIT_REF
-	// TODO get POM info
-  // TODO load Config Files
-  // TODO Switch to QA or PROD
-
 	openshift.withCluster() {
-		withEnv(["PATH+OC=$ocHome"]) {
       stage('Push into Artifactory') {
         configFileProvider([configFile(fileId: 'docker_config.json', targetLocation: "/home/jenkins/.docker/config.json")]) {
           // the ARTIFACTORY_URL also includes the path to store the image in Artifactory
 
-          def current_image_tag = "${params.projectName}:${params.selectedImageTag}"
+          def dev_image_tag = "${params.projectName}:${params.selectedImageTag}"
+          def qa_image_tag = "${dev_imag_tag}-rc-b${currentBuild.number}"
 
-          def results = openshift.raw("image mirror $OCPDEV_REGISTRY_URL/${params.ocpnamespace}/$current_image_tag $ARTIFACTORY_URL/$current_image_tag-rc-b${currentBuild.number} --insecure ")
+          def results = openshift.raw("image mirror $OCPDEV_REGISTRY_URL/${params.ocpnamespace}/$dev_image_tag $ARTIFACTORY_URL/$qa_image_tag --insecure ")
           echo "image mirror results = $results"
         }
       }
-      // TODO create Service, Route, Deployment Config, Image Stream
-      // TODO process CM/SK
-      // TODO Verify Deployment
 
-		} // end withEnv
+      stage('Get Configs from SCM') {
+				// the image has a reference to the git commit's SHA
+        def image_info = openshift.raw("image info $ARTIFACTORY_URL/$qa_image_tag --insecure")
+				def git_ref = (image_info =~ /GIT_REF=[a-z0-9]+/)[0].split("=")[1]
+
+				if(git_ref != "") {
+					// not good, but necessary until we fix our self-signed certificate issue
+					try {
+						checkout([$class: 'GitSCM', branches: [[name: commitHash ]], userRemoteConfigs: [[credentialsId: 'git-sa', url: params.gitUrl]]])
+					} catch (Exception e) {
+						sh "git config http.sslVerify false"
+						checkout([$class: 'GitSCM', branches: [[name: commitHash ]], userRemoteConfigs: [[credentialsId: 'git-sa', url: params.gitUrl]]])
+					}
+				}
+
+      }
+
+			stage("Process Templates") {
+
+			}
+
+			stage("Process CM/SK") {
+
+			}
+
+			stage("Verify Rollout") {
+
+			}
+
 	} // end withCluster
 } //end runPipeline
