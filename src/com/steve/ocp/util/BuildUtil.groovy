0493f01eb537cc34b185b1f99b12c397f7b7b79b
Steve Tran <stran@redhat.com>
2019-09-18 11:34:30 -0400

    Change Build Verification Step
    - There was a bug discovered when there were multiple builds returned
    - We only care about the last one

--- src/com/steve/ocp/util/BuildUtil.groovy
+++ src/com/steve/ocp/util/BuildUtil.groovy
@@ -14,18 +14,39 @@ def start(def buildConfig, def artifactName, def envs) {
 def verify(def buildConfig) {
   def bc = openshift.selector("buildconfig", buildConfig)
   def builds = bc.related('builds')
-  builds.watch {
-    if ( it.count() == 0 ) return false
-    // A robust script should not assume that only one build has been created, so we will need to iterate through all builds.
-    def allDone = true
-    it.withEach {
-      // 'it' is now bound to a Selector selecting a single object for this iteration.  Let's model it in Groovy to check its status.
-      def buildModel = it.object()
-      if ( it.object().status.phase != "Complete" ) {
-        allDone = false
-      }
+
+  // loops through and finds the last build number
+  def latestBuild = -1
+  builds.withEach {
+    def buildNum = it.object().metadata.annotations["openshift.io/build.number"]
+    if(buildNum > latestBuild) {
+      latestBuild = buildNum
     }
-    return allDone;
+  }
+
+  // only wait up to 2 minutes
+  timeout(2) {
+    builds.watch {
+
+      if ( it.count() == 0 ) return false
+      // A robust script should not assume that only one build has been created, so we will need to iterate through all builds.
+      def allDone = true
+      it.withEach {
+
+        // 'it' is now bound to a Selector selecting a single object for this iteration.  Let's model it in Groovy to check its status.
+        def buildModel = it.object()
+        def buildNum = buildModel.metadata.annotations["openshift.io/build.number"]
+        if( buildNum != latestBuild) {
+          // not the latest build, we don't care about this one
+          return
+        }
+
+        if ( it.object().status.phase != "Complete" ) {
+          allDone = false
+        }
+      }
+      return allDone;
+    }
   }
 }
 
